8bfcc18a61abf374d78cfe2b71f8c412c178376d
Yakubu Alhassan <yaqoubdramani@gmail.com>
2024-12-23 12:18:36 +0000

    Update base url

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import StackNavigator from "./navigation/StackNavigator";
-import { API_BASE_URL } from "@env";
+import { REACT_API_BASE_URL } from "@env";
 
 export default function App() {
-  console.log("API Base URL:", API_BASE_URL);
+  console.log("API Base URL:", REACT_API_BASE_URL);
   return (
     <NavigationContainer>
       <StackNavigator />
--- api.js
+++ api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { API_BASE_URL } from "@env";
 
 const api = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: process.env.REACT_API_BASE_URL,
 });
 
 export default api;
